fb1ae51ed7516fba4cbc2bd3f05957af0f736bfb
C00288136 <c00288136@setu.ie>
2024-02-15 10:54:22 +0000

    finish insertCRUD

--- src/CRUDmain.java
+++ src/CRUDmain.java
@@ -1,11 +1,14 @@
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.util.Scanner;
 
 public class CRUDmain {
 
     public static void main(String[] args) {
         Scanner Userin = new Scanner(System.in);
-        insertIntoDbCRUD insert =  new insertIntoDbCRUD();
+        Date currentDate = new Date(System.currentTimeMillis());
+        InsertIntoDbCRUD insert =  new InsertIntoDbCRUD();
         DatabaseConnector databaseConnector = new DatabaseConnector();
         //i can use the base connector but then need the connection class to actual forward the info to the table
         Connection connection = databaseConnector.connect();
@@ -18,10 +21,10 @@ public class CRUDmain {
             switch (choice){
 
                 case 1:
-                    System.out.println("Do you want do insert\ndelete\nview table");
+                    System.out.println("Do you want to \ninsert\ndelete\nview table");
                     String userChoice = Userin.nextLine().toLowerCase();
                     switch (userChoice){
-                case "insert":
+                        case "insert":
                         System.out.println("Inserting data into Employees table:");
                         System.out.print("Enter Last Name: ");
                         String lastName = Userin.nextLine();
@@ -41,10 +44,44 @@ public class CRUDmain {
                         String address = Userin.nextLine();
 
                         insert.insertIntoEmployees(lastName,firstName,age,phoneNo,address);
+                        break;
                     }
+                    break;
                 case 2:
+                    System.out.println("Do you want to \ninsert\ndelete\nview table");
+                    userChoice = Userin.nextLine().toLowerCase();
+                    switch (userChoice) {
+                        case "insert":
+                            System.out.println("Inserting data into Orders table:");
+                            System.out.print("Enter Emp_ID: ");
+                            int Emp_Id = Userin.nextInt();
+
+                            System.out.print("Enter Stock_ID: ");
+                            int Stock_id = Userin.nextInt();
+
+
+                            System.out.print("Enter Total Cost: ");
+                            BigDecimal Total = Userin.nextBigDecimal();
+
+                            System.out.print("Enter Payment Status (paid, pending) : ");
+                            String PayStat = Userin.nextLine().toLowerCase();
+
+                            long delivery_in_2 = 2 * 24 * 60 * 60 * 1000L;
+                            long currentTime = System.currentTimeMillis();
+                            long Delivery = currentTime + delivery_in_2;
+
+                            Date Delivery_date = new Date(Delivery);
+
+                            insert.insertIntoOrders(Emp_Id, Stock_id, currentDate, Total, PayStat, Delivery_date);
+                            break;
+                    }
+                    break;
                 case 3:
+                    break;
+
                 case 4:
+                    break;
+
             }
         }
 
@@ -56,3 +93,4 @@ public class CRUDmain {
 
     }
 }
+
---
+++ src/InsertIntoDbCRUD.java
@@ -0,0 +1,104 @@
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertIntoDbCRUD {
+
+    private DatabaseConnector databaseConnector;
+
+    public InsertIntoDbCRUD(DatabaseConnector databaseConnector) {
+        this.databaseConnector = databaseConnector;
+    }
+
+    public InsertIntoDbCRUD() {
+
+    }
+
+    public void insertIntoTable(String tableName, String[] columns, Object[] values) {
+        Connection connection = null;
+        PreparedStatement pstat = null;
+        int i = 0;
+
+        try {
+            // Get connection from the DatabaseConnector
+            connection = databaseConnector.connect();
+
+            // Create Prepared Statement for inserting data into the specified table
+            StringBuilder queryBuilder = new StringBuilder("INSERT INTO ")
+                    .append(tableName)
+                    .append(" (");
+
+            for (int j = 0; j < columns.length; j++) {
+                queryBuilder.append(columns[j]);
+                if (j < columns.length - 1) {
+                    queryBuilder.append(", ");
+                }
+            }
+
+            queryBuilder.append(") VALUES (");
+
+            for (int j = 0; j < values.length; j++) {
+                queryBuilder.append("?");
+                if (j < values.length - 1) {
+                    queryBuilder.append(", ");
+                }
+            }
+
+            queryBuilder.append(")");
+
+            String query = queryBuilder.toString();
+            pstat = connection.prepareStatement(query);
+
+            // Set values for each parameter
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] instanceof String) {
+                    pstat.setString(j + 1, (String) values[j]);
+                } else if (values[j] instanceof Integer) {
+                    pstat.setInt(j + 1, (Integer) values[j]);
+                } else if (values[j] instanceof Long) {
+                    pstat.setLong(j + 1, (Long) values[j]);
+                } else if (values[j] instanceof BigDecimal) {
+                    pstat.setBigDecimal(j + 1, (BigDecimal) values[j]);
+                } else if (values[j] instanceof Date){
+                    {pstat.setDate(j + 1, (Date) values[j]);}
+                }
+            }
+
+            // Insert data into the table
+            i = pstat.executeUpdate();
+            System.out.println(i + " record successfully added to " + tableName + ".");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        } finally {
+            try {
+                if (pstat != null) {
+                    pstat.close();
+                }
+                // Disconnect from the DatabaseConnector
+                databaseConnector.disconnect();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public void insertIntoEmployees(String lastName, String firstName, int age, long phoneNum, String address) {
+        String[] columns = {"LastName", "FirstName", "Age", "Phone_No", "Address"};
+        Object[] values = {lastName, firstName, age, phoneNum, address};
+        insertIntoTable("Employees", columns, values);
+    }
+
+    public void insertIntoStockItems(String name, int quantity, BigDecimal unitPrice, BigDecimal salePrice, int supplierId, int aisleNum) {
+        String[] columns = {"Name", "Quantity", "UnitPrice", "SalePrice", "SupplierId", "AisleNum"};
+        Object[] values = {name, quantity, unitPrice, salePrice, supplierId, aisleNum};
+        insertIntoTable("Stock_Items", columns, values);
+    }
+
+    public void insertIntoOrders(int Emp_ID,int Stock_ID,Date date,BigDecimal TotalCost, String PaymentStat, Date delivery ){
+        String[] columns = {"Emp_ID","Stock_ID","OrderDate","TotalCost","PaymentStatus","Est_Delivery"};
+        Object[] values = {Emp_ID,Stock_ID,date,TotalCost,PaymentStat,delivery};
+        insertIntoTable("Orders", columns, values);
+    }
+}
