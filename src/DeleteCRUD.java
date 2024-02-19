4ef3f52fd1339e504167dbcac7bf265f93a66081
C00288136 <C00288136@setu.ie>
2024-02-19 11:32:16 +0000

    Added Delete CRUD

---
+++ src/DeleteCRUD.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+
+public class DeleteCRUD {
+
+    private DatabaseConnector databaseConnector;
+
+    public DeleteCRUD(DatabaseConnector databaseConnector) {
+        this.databaseConnector = databaseConnector;
+    }
+
+    public DeleteCRUD() {}
+
+    public void deleteFromTable(String tableName, int ID) {
+        Connection connection = null;
+
+        try {
+            connection = databaseConnector.connect();
+            DatabaseMetaData metaData = connection.getMetaData();
+
+            String idColumnName = null;
+            ResultSet resultSet = metaData.getPrimaryKeys(null, null, tableName);
+
+            if (resultSet.next()) {
+                idColumnName = resultSet.getString("COLUMN_NAME");
+            } else {
+                System.out.println("Table " + tableName + " doesn't have a primary key");
+                return;
+            }
+
+            String deleteQuery = "DELETE FROM " + tableName + " WHERE " + idColumnName + " = ?";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+                preparedStatement.setInt(1, ID);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Record deleted successfully.");
+                } else {
+                    System.out.println("Record with ID " + ID + " not found.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
