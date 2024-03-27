413fa599a50dc3992e4b51d7f90476ff6abdf502
PIanxty <C00287277@setu.ie>
2024-03-27 11:09:59 +0000

    jlist

---
+++ src/Jlist.java
@@ -0,0 +1,19 @@
+// This class will be used for dynamically fetching and displaying existing records
+
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+
+
+
+public class Jlist extends JPanel {
+    Connection connection;
+    String tableName;
+
+    public Jlist(String tableName)
+    {
+        this.tableName = tableName;
+    }
+
+}
