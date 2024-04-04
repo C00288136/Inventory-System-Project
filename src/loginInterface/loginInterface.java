71937dde72372267441ecff1310b0b2310a0e424
Cheetos <C00288136@setu.ie>
2024-04-04 18:56:49 +0100

    create logo

--- src/loginInterface/loginInterface.java
+++ src/loginInterface/loginInterface.java
@@ -3,8 +3,6 @@ package loginInterface;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class loginInterface extends JPanel {
 
@@ -13,25 +11,67 @@ public class loginInterface extends JPanel {
     JLabel passwordLabel = new JLabel("Password");
     public JPasswordField passwordText =  new JPasswordField();
     public JButton loginButton = new JButton("Login");
-    public loginInterface(){
-        userLabel.setBounds(10,20,80,25);
-        add(userLabel);
 
-        userText.setBounds(100,20,165,25);
-        add(userText);
 
-        passwordLabel.setBounds(10,50,80,25);
-        add(passwordLabel);
 
-        passwordText.setBounds(100,50,165,25);
-        add(passwordText);
 
-        loginButton.setBounds(10,80,80,25);
+    int panelWidth = getWidth()/2;
 
-        add(loginButton);
-        setLayout(null);
+    public loginInterface() {
+        setLayout(new GridLayout(1,2));
+
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(null); // Using null layout for precise positioning
+
+        Font font = new Font(Font.SANS_SERIF,Font.PLAIN,16);
+        userLabel.setFont(font);
+        userText.setFont(font);
+        passwordLabel.setFont(font);
+        loginButton.setFont(font);
+        JPanel rightPanel = new JPanel();
+        rightPanel.setBackground(Color.LIGHT_GRAY); // Set the background color as needed
+        add(rightPanel); // Place it on the right
+
+
+        loginPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10); // Add some padding
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        loginPanel.add(userLabel,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        userText.setPreferredSize(new Dimension(200,25));
+        loginPanel.add(userText,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+
+        loginPanel.add(passwordLabel,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        passwordText.setPreferredSize(new Dimension(200,25));
+        loginPanel.add(passwordText,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+
+        loginPanel.add(loginButton,gbc);
+
+        add(loginPanel); // Place loginPanel on the left
+
+
+
         setVisible(true);
-
     }
-}
 
+
+}
